8a7b6739c077d84ad7db15c2c14d13f52eb2abac
MTPaulette <mayoguepaulette@gmail.com>
2024-07-27 12:42:37 +0100

    before deploy

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ const app = createApp(App)
 //app.use(Vuex)
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.defaults.baseURL = 'https://fidelityapi.brain-booster.net'
 
 const axiosConfig = {
   // 'Content-Type': 'application/json',
